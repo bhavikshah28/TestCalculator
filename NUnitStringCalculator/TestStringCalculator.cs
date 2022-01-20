cab7771d940a21c7ec471d367ac6c0ebc24c2e14
bhavik.s <bhavik.shah@intelegain.com>
2022-01-20 20:47:39 +0530

    Add project files.

---
+++ NUnitStringCalculator/TestStringCalculator.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using NUnit.Framework;
+using StringCalculator;
+
+namespace NUnitStringCalculator
+{
+    [TestFixture]
+    public class TestStringCalculator
+    {
+        Calculator calc;
+
+        #region Setup
+        [SetUp]
+        public void setup()
+        {
+            calc = new Calculator();
+        }
+        #endregion
+        #region Testing Scenario
+
+        /// <summary>
+        /// Test Empty String
+        /// </summary>
+        [Test]
+        public void TestEmptyString() {
+            var result = calc.Add("");
+            Assert.AreEqual(0, result);
+        }
+
+        /// <summary>
+        /// Test Single Number
+        /// </summary>
+        [Test]
+        public void TestAddSingleNumber()
+        {
+            var result = calc.Add("1");
+            Assert.AreEqual(1, result);
+        }
+
+        /// <summary>
+        /// Test Sum of Two Numbers
+        /// </summary>
+        [Test]
+        public void TestAddSumOfTwoNumbers()
+        {
+            var result = calc.Add("7,8");
+            Assert.AreEqual(15, result);
+        }
+
+        /// <summary>
+        /// Test SUm of Two Larger Numbers
+        /// </summary>
+        [Test]
+        public void TestAddSumOfTwoLargerNumbers_55()
+        {
+            var result = calc.Add("30,25");
+            Assert.AreEqual(55, result);
+        }
+
+        /// <summary>
+        /// Test New Line
+        /// </summary>
+        [Test]
+        public void TestAddNewLine()
+        {
+            var result = calc.Add("\n");
+            Assert.AreEqual(0, result);
+        }
+
+        /// <summary>
+        /// Test sum of Two Number sperated by new line
+        /// </summary>
+        [Test]
+        public void TestAddTwoNumberDelimiterofNewLine()
+        {
+            var result = calc.Add("4\n5");
+            Assert.AreEqual(9, result);
+        }
+
+        /// <summary>
+        /// Added custom delimiter of //X
+        /// </summary>
+        [Test]
+        public void TestAddCustomDelimiter()
+        {
+            var result = calc.Add("//X\n5A1");
+            Assert.AreEqual(6, result);
+        }
+
+        /// <summary>
+        /// Added custom delimiter of //D
+        /// </summary>
+        [Test]
+        public void TestAddCustomDelimiterLargerNumber()
+        {
+            var result = calc.Add("//D\n20B8");
+            Assert.AreEqual(28, result);
+        }
+
+        /// <summary>
+        /// Negative Number Test
+        /// </summary>
+        [Test]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.ExpectedException(typeof(ApplicationException))]
+        public void TestSingleNegativeNumber()
+        {
+            var result = calc.Add("-1");
+        }
+
+        /// <summary>
+        /// Multiple Negative Number Test
+        /// </summary>
+        [Test]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.ExpectedException(typeof(ApplicationException))]
+        public void TestMultipleNegativeNumbers()
+        {
+            var result = calc.Add("-1,-2");
+        }
+
+        /// <summary>
+        /// Test Max Number
+        /// </summary>
+        [Test]
+        public void TestValidateMaxNumber()
+        {
+            var result = calc.Add("2,1001");
+            Assert.AreEqual(2, result);
+        }
+
+        /// <summary>
+        /// Test Multiple Character
+        /// </summary>
+        [Test]
+        public void TestMultipleCharactersUsed()
+        {
+            var result = calc.Add("//[AB][bc]\n3AB2bc4");
+            Assert.AreEqual(9, result);
+        }
+        #endregion
+    }
+}
---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        #region Constants
+        private const int DEF_NUMBER = 0;
+        private const string CUSTOM_DELIMITER = "//";
+        private readonly List<string> delimiters = new List<string>() { ",", "\n" };
+        private const int MAX_NUMBER = 1000;
+        #endregion
+        #region Addition Method
+        /// <summary>
+        /// Common method to accept input as number and return sum of string integers
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        public int Add(string numbers)
+        {
+            int sumOfNumbers = 0;
+            try
+            {
+                if (string.IsNullOrEmpty(numbers))
+                    return DEF_NUMBER;
+
+                if (numbers.Contains(CUSTOM_DELIMITER))
+                {
+                    numbers = AddCustomDelimiter(numbers);
+                }
+
+                sumOfNumbers = CalculateSum(numbers);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return sumOfNumbers;
+        }
+        #endregion
+        #region Common Methods
+        /// <summary>
+        /// Adding Delimiters
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        private string AddCustomDelimiter(string numbers)
+        {
+            string[] customDelimiters = { CUSTOM_DELIMITER, "[", "]" };
+            var customDelimiter = numbers.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries).FirstOrDefault();
+
+            numbers = numbers.Substring(customDelimiter.Length, numbers.Length - customDelimiter.Length);
+            var allDelimiterDefaults = customDelimiter.Split(customDelimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach(var delimit in allDelimiterDefaults)
+            {
+                delimiters.Add(delimit);
+            }
+            return numbers;
+        }
+
+        /// <summary>
+        /// Calculate Sum
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        private int CalculateSum(string numbers)
+        {
+            int number = 0;
+            List<int> lstnumbers = new List<int>();
+            try
+            {
+                var seperatenumber = numbers.Split(delimiters.ToArray(), StringSplitOptions.RemoveEmptyEntries);
+
+                // Iterate lopp of comma or new line seperated and inserted
+                for (int i = 0; i < seperatenumber.Length; i++)
+                {
+                    number = int.Parse(seperatenumber[i]);
+
+                    // Negative Number Validation 
+                    if (number < 0)
+                        throw new ApplicationException("Number can not be Negative");
+
+                    // Max Number adding entry
+                    if (number <= MAX_NUMBER)
+                        lstnumbers.Add(number);
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            return lstnumbers.Count() > 0 ? lstnumbers.Sum() : 0;
+        }
+        #endregion
+    }
+}
